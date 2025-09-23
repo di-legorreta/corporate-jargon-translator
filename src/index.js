239adc29521e1956a917da58d4492673b57f388b
di-legorreta <114834384+di-legorreta@users.noreply.github.com>
2025-09-23 20:07:22 +0200

    added basic text handling and typewriter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.querySelector(".toggle").addEventListener("click", () => {
    document.body.classList.toggle("genz");
 });
+
+function translateJargon(event) {
+   event.preventDefault();
+
+   new Typewriter("#result", {
+      strings: "That task doesn't fall under my responsability",
+      autoStart: true,
+      delay: "natural",
+      cursor: "",
+   });
+}
+
+let inputEl = document.querySelector("#translate");
+inputEl.addEventListener("click", translateJargon);
